241d7fe3bb7577d8de37fcddb71bee2c8184c18f
adil <adil.souirite@gmail.com>
2020-05-06 18:28:11 +0100

    add timesheet form

--- PayCalculator/Forms/FormTimeSheet.Designer.cs
+++ PayCalculator/Forms/FormTimeSheet.Designer.cs
@@ -28,39 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtusername = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.dgtimesheet = new System.Windows.Forms.DataGridView();
+            this.Columnweek = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columnbasic = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columnstdot = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columnhrot = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columnnight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columntravel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columnholiday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columnbankholiday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columnillness = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columnchangeover = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columntotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblselect = new System.Windows.Forms.Label();
+            this.btnexport = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgtimesheet)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnupdate
             // 
-            this.button1.Location = new System.Drawing.Point(188, 35);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Ok";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnupdate.Location = new System.Drawing.Point(188, 15);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(75, 42);
+            this.btnupdate.TabIndex = 9;
+            this.btnupdate.Text = "UPDATE DATA";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox2
+            // txtpassword
             // 
-            this.textBox2.Location = new System.Drawing.Point(82, 37);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 8;
-            this.textBox2.Text = "Ziad0412";
+            this.txtpassword.Location = new System.Drawing.Point(82, 37);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
+            this.txtpassword.Size = new System.Drawing.Size(100, 20);
+            this.txtpassword.TabIndex = 8;
+            this.txtpassword.Text = "Ziad0412";
             // 
-            // textBox1
+            // txtusername
             // 
-            this.textBox1.Location = new System.Drawing.Point(82, 15);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 7;
-            this.textBox1.Text = "ASOUIRITE1";
+            this.txtusername.Location = new System.Drawing.Point(82, 15);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(100, 20);
+            this.txtusername.TabIndex = 7;
+            this.txtusername.Text = "ASOUIRITE1";
             // 
             // label2
             // 
@@ -80,18 +99,144 @@
             this.label1.TabIndex = 5;
             this.label1.Text = "Password";
             // 
+            // dgtimesheet
+            // 
+            this.dgtimesheet.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgtimesheet.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Columnweek,
+            this.Columnbasic,
+            this.Columnstdot,
+            this.Columnhrot,
+            this.Columnnight,
+            this.Columntravel,
+            this.Columnholiday,
+            this.Columnbankholiday,
+            this.Columnillness,
+            this.Columnchangeover,
+            this.Columntotal});
+            this.dgtimesheet.Location = new System.Drawing.Point(12, 63);
+            this.dgtimesheet.Name = "dgtimesheet";
+            this.dgtimesheet.Size = new System.Drawing.Size(776, 375);
+            this.dgtimesheet.TabIndex = 10;
+            // 
+            // Columnweek
+            // 
+            this.Columnweek.HeaderText = "Week";
+            this.Columnweek.Name = "Columnweek";
+            // 
+            // Columnbasic
+            // 
+            this.Columnbasic.HeaderText = "Basic";
+            this.Columnbasic.Name = "Columnbasic";
+            // 
+            // Columnstdot
+            // 
+            this.Columnstdot.HeaderText = "Standard Overtime";
+            this.Columnstdot.Name = "Columnstdot";
+            // 
+            // Columnhrot
+            // 
+            this.Columnhrot.HeaderText = "High Rate Overtime";
+            this.Columnhrot.Name = "Columnhrot";
+            // 
+            // Columnnight
+            // 
+            this.Columnnight.HeaderText = "Nights";
+            this.Columnnight.Name = "Columnnight";
+            // 
+            // Columntravel
+            // 
+            this.Columntravel.HeaderText = "Travel";
+            this.Columntravel.Name = "Columntravel";
+            // 
+            // Columnholiday
+            // 
+            this.Columnholiday.HeaderText = "Holiday";
+            this.Columnholiday.Name = "Columnholiday";
+            // 
+            // Columnbankholiday
+            // 
+            this.Columnbankholiday.HeaderText = "Bank Holiday";
+            this.Columnbankholiday.Name = "Columnbankholiday";
+            // 
+            // Columnillness
+            // 
+            this.Columnillness.HeaderText = "Illness";
+            this.Columnillness.Name = "Columnillness";
+            // 
+            // Columnchangeover
+            // 
+            this.Columnchangeover.HeaderText = "Changeover";
+            this.Columnchangeover.Name = "Columnchangeover";
+            // 
+            // Columntotal
+            // 
+            this.Columntotal.HeaderText = "Total";
+            this.Columntotal.Name = "Columntotal";
+            // 
+            // lblselect
+            // 
+            this.lblselect.AutoSize = true;
+            this.lblselect.Location = new System.Drawing.Point(387, 44);
+            this.lblselect.Name = "lblselect";
+            this.lblselect.Size = new System.Drawing.Size(28, 13);
+            this.lblselect.TabIndex = 11;
+            this.lblselect.Text = "data";
+            // 
+            // btnexport
+            // 
+            this.btnexport.Location = new System.Drawing.Point(713, 15);
+            this.btnexport.Name = "btnexport";
+            this.btnexport.Size = new System.Drawing.Size(75, 41);
+            this.btnexport.TabIndex = 12;
+            this.btnexport.Text = "Export To Excel";
+            this.btnexport.UseVisualStyleBackColor = true;
+            this.btnexport.Click += new System.EventHandler(this.btnexport_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(454, 15);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(243, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(294, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Week Selected :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(361, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Progress :";
+            // 
             // FormTimeSheet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnexport);
+            this.Controls.Add(this.lblselect);
+            this.Controls.Add(this.dgtimesheet);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "FormTimeSheet";
             this.Text = "Time Sheet";
+            ((System.ComponentModel.ISupportInitialize)(this.dgtimesheet)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -99,10 +244,28 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtusername;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columnweek;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columnbasic;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columnstdot;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columnhrot;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columnnight;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columntravel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columnholiday;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columnbankholiday;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columnillness;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columnchangeover;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columntotal;
+        private System.Windows.Forms.Label lblselect;
+        private System.Windows.Forms.Button btnexport;
+        public System.Windows.Forms.DataGridView dgtimesheet;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- PayCalculator/Forms/FormTimeSheet.cs
+++ PayCalculator/Forms/FormTimeSheet.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
 
 namespace PayCalculator.Forms
 {
@@ -19,7 +22,112 @@ namespace PayCalculator.Forms
 
         private void button1_Click(object sender, EventArgs e)
         {
+            FirefoxOptions Options = new FirefoxOptions();
+            Options.AddArgument("--headless");
+            IWebDriver drive = new FirefoxDriver(Options);
+            drive.Url = "https://atg.ceccontrols.com/wts/index.jsp";
+            string username = txtusername.Text;
+            string password = txtpassword.Text;
 
+            drive.FindElement(By.Id("username")).SendKeys(username);
+            drive.FindElement(By.Id("j_password")).SendKeys(password);
+            drive.FindElement(By.Id("submitbtn")).Click();
+
+            drive.FindElement(By.XPath("//td[contains(text(),'HISTORY')]")).Click();
+            drive.FindElement(By.XPath("//td[contains(text(),'APPROVED')]")).Click();
+
+            var table = drive.FindElement(By.TagName("table"));
+            var rows = table.FindElements(By.ClassName("lineitem")).Count;
+            var rows_count = rows / 7;
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = rows_count;
+
+            for (int i = 2; i < rows_count; i++) //rows_count
+            {
+                progressBar1.Value = i;
+                Thread.Sleep(300);
+                //*[@id="layer2"]/table/tbody/tr[12]/td[2]
+                lblselect.Text = drive.FindElement(By.XPath("//*[@id='layer2']/table/tbody/tr[" + i + "]/td[2]")).Text;
+                drive.FindElement(By.XPath("//*[@id='layer2']/table/tbody/tr[" + i + "]/td[2]")).Click();
+
+                lblselect.Update();
+                try
+                {
+                    drive.FindElement(By.XPath("//td[contains(text(),'Hours Summary')]")).Click();
+                    Thread.Sleep(500);
+                    drive.SwitchTo().Window(drive.WindowHandles[1]);
+                    Thread.Sleep(500);
+
+                    string timesheet = drive.FindElement(By.XPath("//html/body/pre")).Text;
+                    
+                    timesheet = lblselect.Text + "\r\n" + timesheet;
+                    timesheet = timesheet.Replace("Basic:", "");
+                    timesheet = timesheet.Replace("Standard Overtime:", "");
+                    timesheet = timesheet.Replace("High Rate Overtime:", "");
+                    timesheet = timesheet.Replace("Nights:", "");
+                    timesheet = timesheet.Replace("Travel:", "");
+                    timesheet = timesheet.Replace("Holiday:", "");
+                    timesheet = timesheet.Replace("Bank", "");
+                    timesheet = timesheet.Replace("Bank Holiday:", "");
+                    timesheet = timesheet.Replace("Illness:", "");
+                    timesheet = timesheet.Replace("Changeover:", "");
+                    timesheet = timesheet.Replace("Total:", "");
+                    dgtimesheet.Rows.Add(timesheet.Split('\n'));
+
+                    Thread.Sleep(500);
+                    drive.Close();
+                    drive.SwitchTo().Window(drive.WindowHandles[0]);
+                    drive.Navigate().Back();
+                    lblselect.Text = "";
+                }
+                catch
+                {
+                    drive.Navigate().Back();
+                }
+            }
+            drive.Close();
         }
+
+        private void btnexport_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = "C:";
+            saveFileDialog1.Title = "Export TimeSheet to Excel";
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.Filter = "Excel Files 2003|*.xls|Excel Files 2007|*.xlsx";
+
+            Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+            app.Visible = false;
+
+            Microsoft.Office.Interop.Excel.Workbook wb = app.Workbooks.Add(Microsoft.Office.Interop.Excel.XlWBATemplate.xlWBATWorksheet);
+            Microsoft.Office.Interop.Excel.Worksheet ws = (Microsoft.Office.Interop.Excel.Worksheet)wb.ActiveSheet;
+            app.Application.Worksheets.Add(Type.Missing); 
+            if (saveFileDialog1.ShowDialog() != DialogResult.Cancel) 
+            {
+                //header
+                for (int i = 0; i < dgtimesheet.Columns.Count; i++)
+                {
+                    ws.Cells[1, i + 1] = dgtimesheet.Columns[i].HeaderText;
+                }
+                //content
+                for (int i = 0; i < dgtimesheet.Rows.Count; i++)
+                {
+                    if (dgtimesheet.Rows.Count > 2)
+                    {
+                        for (int j = 0; j < dgtimesheet.Columns.Count; j++)
+                        {
+                            ws.Cells[i + 2, j + 1] = dgtimesheet.Rows[i].Cells[j].ToString();
+                        }
+                    }
+                }
+                ws.Name = "TimeSheet data";
+
+                wb.SaveCopyAs(saveFileDialog1.FileName.ToString());
+                //wb.SaveAs(true);
+                app.Quit();
+            }
+            
+        }
+        
     }
 }
